bc30fa40d18a60e3be489b383088979c9a579077
sahulhameed.s <sahulhameed.s@bthinq.com>
2023-08-08 12:37:22 +0530

    project setup

---
+++ Product.Microservice/Controllers/ProdcutController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Product.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdcutController : ControllerBase
+    {
+        // GET: api/<ProdcutController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProdcutController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProdcutController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProdcutController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProdcutController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
